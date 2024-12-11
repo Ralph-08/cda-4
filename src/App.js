41f60aac322f5b76743ed16275b4eeaf60c30eec
Rafael Ramos <junior07.rrn@hotmail.com>
2024-12-10 20:27:39 -0500

    Add home page to App.js file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Warehouse from "./pages/Warehouse";
 import Inventory from "./pages/Inventory";
+import Home from "./pages/Home";
 import "./App.scss";
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
       <div className="App">
         <Header />
         <Routes>
-          <Route path={"/"} element={<Warehouse />} />
+          <Route path={"/"} element={<Home />} />
           <Route path={"warehouses"} element={<Warehouse />} />
           <Route path={"inventory"} element={<Inventory />} />
         </Routes>
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import DonutSpinner from "../components/DonutSpinner";
+
+const Home = () => {
+  return (
+    <div className="App">
+      <h1>Home</h1>
+      <p>Feel free to explore other pages!</p>
+      <DonutSpinner />
+    </div>
+  );
+};
+
+export default Home;
