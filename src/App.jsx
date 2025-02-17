efa5ac075dbde8c139e2fa0b4c055ed758968267
Rafael Ramos <junior07.rrn@hotmail.com>
2025-02-17 13:48:47 -0500

    Create customer list page

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import ActiveOrders from "./pages/ActiveOrders/ActiveOrders";
 import OrderHistory from "./pages/OrderHistory/OrderHistory";
 import Orders from "./pages/Orders/Orders";
 import EditOrder from "./pages/EditOrder/EditOrder";
+import CustomerList from "./pages/CustomerList/CustomerList";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Routes>
-          <Route path={"/"} element={<Home />} />
-          <Route path={"/ordenes"} element={<Orders />} />
-          <Route path={"/ordenes-activas"} element={<ActiveOrders />} />
-          <Route path={"/ordenes-activas:orderId"} element={<EditOrder />} />
-          <Route path={"/order-history"} element={<OrderHistory />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/ordenes" element={<Orders />} />
+          <Route path="/ordenes-activas" element={<ActiveOrders />} />
+          <Route path="/ordenes-activas:orderId" element={<EditOrder />} />
+          <Route path="/order-history" element={<OrderHistory />} />
+          <Route path="/lista" element={<CustomerList />} />
         </Routes>
       </div>
     </BrowserRouter>
